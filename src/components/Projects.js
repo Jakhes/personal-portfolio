7d2638cf9aff3909d4d0607b68f0c9e1108320ae
Braumble <dean.schmitz@schmitzbauer.de>
2024-03-23 14:14:58 +0100

    Adding Projects Component

---
+++ src/components/Projects.js
@@ -0,0 +1,72 @@
+import { Col, Container, Nav, Row, Tab } from "react-bootstrap";
+
+export const Projects = () => {
+    const projects = [
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+    ];
+
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>Projects</h2>
+                        <p>Lorem Ipsum is a simple dummy text.</p>
+                        <Tab.Container>
+                            <Nav variant="pills" defaultActiveKey="first">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">
+                                        Tab One
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">
+                                        Tab Two
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third">
+                                        Tab Three
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <Row>
+                                        {projects.map((project, index) => {
+                                            return <p>{project.title}</p>;
+                                        })}
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    <Row>
+                                        {projects.map((project, index) => {
+                                            return <p>{project.title}</p>;
+                                        })}
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="third">
+                                    Nice Content
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+};
